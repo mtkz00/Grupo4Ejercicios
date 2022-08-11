a0de8dd22e416603090c0eefdebec2ffdc9a3cd5
Bautista Ceriani <90162406+mtkz00@users.noreply.github.com>
2022-08-11 17:53:36 -0300

    Add files via upload

---
+++ ejercicio06.py
@@ -0,0 +1,25 @@
+"""
+6)       Crear una tupla en Python con el nombre de “Historial2” la cual contenga los siguientes valores:
+
+          23500, 5960, 2300, 10200, 8900
+
+Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Frida”. Calcular el monto total gastado a lo largo del tiempo por atención de “Frida”. Crear una función que cuente cuantos gastos fueron superiores a 5000 mostrando  el número calculado en pantalla.
+
+"""
+
+Historial2 = (23500, 5960, 2300, 10200, 8900)
+
+x = len(Historial2)
+i = 0
+suma = []
+
+while i < x:
+    
+    if Historial2[i] > 5000:
+        suma.append(Historial2[i])
+    i += 1
+    
+    
+print (sum(suma))
+
+
